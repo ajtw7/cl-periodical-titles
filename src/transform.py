10dc3687ac4694c3b532454ad21512b04ffce7bd
ajtw7 <andrew.worsley91@gmail.com>
2025-07-25 14:04:16 -0400

    WIP: generating random ISSN numbers.

--- src/transform.py
+++ src/transform.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import re
+import requests
+import random
 
 
 # Function to standardize column names by replacing underscores with spaces and capitalizing
@@ -11,6 +13,14 @@ def standardize_column_names(df):
 # Function to create a new column 'Id (no prefix)' by removing the first 5 characters from 'Id'
 def create_unique_id(df, id_prefix_length):
     df['Unique Id'] = df['Id'].apply(lambda x: x[id_prefix_length:] if isinstance(x, str) else x)
+    return df
+
+def create_issn(df):
+    # Check if 'Issn' column exists, if not create it
+    if 'Issn' not in df.columns:
+        df['Issn'] = None
+    # Generate ISSN numbers directly in the 'Issn' column for missing, null, or empty values
+    df['Issn'] = df['Issn'].apply(lambda x: f"{random.randint(1000, 9999)}-{random.randint(1000, 9999)}" if pd.isnull(x) or x == '' else x)
     return df
 
 # Function to drop unnecessary fields from the DataFrame
@@ -32,12 +42,22 @@ def convert_dates(df):
     # Rename column
     return df
 
+def drop_empty_columns(df, threshold=0.5):
+    threshold = len(df) * threshold  # Calculate the threshold based on the number of rows
+    df = df.dropna(thresh=threshold, axis=1)
+    #  print the unique ids of the columns that were dropped
+    dropped_columns = df.columns[df.isnull().mean() > threshold].tolist()
+    if dropped_columns:
+        print("Dropped columns due to high missing data:", dropped_columns)
+    else:
+        print("No columns dropped due to missing data.")
+    return df
 
 # Function to handle missing values in specific columns
 def handle_missing(df):
-    # Replace empty 'Description' with 'No Description'
+   
     # df['Description'] = df['Description'].replace('', 'No Description')
-    df['Issn'] = df['Issn'].replace('', None)
+    # df['Issn'] = df['Issn'].replace('', None)
     
     # Fill empty 'Start Year' with year from 'Start Date' if available
     if 'Start Year' in df.columns and 'Start Date' in df.columns:
@@ -51,21 +71,12 @@ def handle_missing(df):
                     pass
     return df
 
-# Function to validate ISSN format
-def validate_issn(df):
-    issn_pattern = re.compile(r'^\d{4}-\d{3}[\dX]$')
-    df['issn_valid'] = df['Issn'].apply(lambda x: bool(issn_pattern.match(str(x))) if x else False)
-    return df
 
 # Function to remove duplicate entries based on 'id' column
 def remove_duplicates(df):
     df = df.drop_duplicates(subset=['Id'])
     return df
 
-# Function to truncate 'id' field by removing a specified prefix length
-# def truncate_ids(df, prefix_length):
-#     df['Id'] = df['Id'].apply(lambda x: x[prefix_length:] if isinstance(x, str) else x)
-#     return df
 
 # Function to clean 'place' field by removing digits and extra spaces
 def clean_fields(df):
@@ -79,9 +90,13 @@ def trim_whitespace(df):
     return df
 
 # Function to standardize place names using a mapping dictionary
-# def standardize_places(df, place_mapping):
-#     df['place'] = df['place'].replace(place_mapping)
-#     return df
+def standardize_places(df):
+    # Print unique entries in the 'Place' column
+    if 'Place' in df.columns:
+        unique_places = df['Place'].unique()
+        print("Unique entries in 'Place' column:", unique_places)
+    # df['place'] = df['place'].replace(place_mapping)
+    return df
 
 
 
@@ -89,12 +104,13 @@ def trim_whitespace(df):
 def clean_data(df, config):
     df = standardize_column_names(df)
     df = create_unique_id(df, config['id_prefix_length'])
+    df = create_issn(df)
     df = drop_fields(df, ['Extent', 'Description'])
+    df = drop_empty_columns(df, config.get('empty_column_threshold', 0.5))
     df = convert_dates(df)
     df = handle_missing(df)
-    df = validate_issn(df)
     df = remove_duplicates(df)
     df = clean_fields(df)
     df = trim_whitespace(df)
-    # df = standardize_places(df, config['place_mapping'])
+    df = standardize_places(df)
     return df
